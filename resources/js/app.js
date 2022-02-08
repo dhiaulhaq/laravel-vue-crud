2454c83f5d15ceb024c9e7ad4b79ff6cf805d41f
Alwan Alyafi Mulyawan <alwanalyafi7@gmail.com>
2022-02-08 20:04:19 +0700

    fixing vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,34 +4,13 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
- require('./bootstrap');
- window.Vue = require('vue');
- 
- import App from './App.vue';
- import VueAxios from 'vue-axios';
- import * as VueRouter from 'vue-router';
- import axios from 'axios';
- import {
-     routes
- } from './routes';
- 
- /**
-  * Next, we will create a fresh Vue application instance and attach it to
-  * the page. Then, you may begin adding components to this application
-  * or customize the JavaScript scaffolding to fit your unique needs.
-  */
- 
- 
- const router = new VueRouter({
-     mode: 'history',
-     routes: routes
- });
- 
- const app = new Vue({
-     el: '#app',
-     router: router,
-     render: h => h(App),
- });
+import {
+    createApp
+} from 'vue'
+import HelloWorld from './components/AllUsers.vue';
 
- app.use(VueRouter);
- app.use(VueAxios, axios);
+const app = createApp({});
+app.component('hello-world', HelloWorld)
+    .mount('#app');
+
+require('./bootstrap');
